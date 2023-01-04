e8a2e90276d698adcd1153ecca7ca707b0985879
raul <rcr_032@hotmail.com>
2023-01-04 14:56:57 +0100

    Hooks

--- ob-react/src/App.js
+++ ob-react/src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-// import Greeting from './components/pure/greeting';
-// import GreetingF from './components/pure/greetingF';
-// import TaskListComponent from './components/container/taskListComponent';
+import Greeting from './components/pure/greeting';
+import GreetingF from './components/pure/greetingF';
+import TaskListComponent from './components/container/taskListComponent';
 import ContactoA from './components/container/contactoA';
 import { Contacto } from './models/contacto.class';
 import Ejemplo1 from './hooks/Ejemplo1';
@@ -12,16 +12,33 @@ import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
   
+  const contactoPrueba= new Contacto('Raúl', 'Carneros Rodríguez', 'rcr@hotmail.comm', false);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        {/**Componente propio Greeting.jsx */}
+        {/* <Greeting name={'Raúl'}></Greeting> */}
+        {/* <GreetingF name={'Raúl'}></GreetingF> */}
         
-        <Ejemplo4 nombre='Raúl'>
+        {/**Componente de ejemplo funcional */}
+        {/* <TaskListComponent></TaskListComponent> */}
+
+        {/* <ContactoA contacto={contactoPrueba}></ContactoA> */}
+
+        {/**Ejemplo de HOOKS */}
+
+        {/* <Ejemplo1></Ejemplo1> */}
+        {/* <Ejemplo2></Ejemplo2> */}
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+        
+        {/* <Ejemplo4 nombre='Raúl'> */}
         {/** Todo lo que hay aquí, es tratado como props.children  */}
-          <h3>Contenido del props.children</h3>
-          <p>Hola mundo</p>
-        </Ejemplo4>
+          {/* <h3>Contenido del props.children</h3>
+          <p>Hola mundo</p> */}
+        {/* </Ejemplo4> */}
 
       </header>
     </div>
